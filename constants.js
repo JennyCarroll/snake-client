5416a3fb32b984b9a396a27ecd87f64161eea2dc
Jenny Carroll <jennygc@telus.net>
2023-02-16 02:26:18 +0000

    add a constants.js and included an object of my moves (just the ones that are strings since socket.write is the output ofnet.createConnectiond and so connection.write expects a string

---
+++ constants.js
@@ -0,0 +1,16 @@
+// CONSTANT_VALUES
+const IP = "165.227.47.243";
+const PORT = 50541;
+const MOVES = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+  " ": "Say: Move!",
+};
+
+module.exports = {
+  IP,
+  PORT,
+  MOVES,
+};
--- input.js
+++ input.js
@@ -1,27 +1,29 @@
 let connection;
+const { MOVES } = require("./constants");
 
 //created a function that specifies what happens when data is received from stdin
 const handleUserInput = function (userInput) {
-  console.log("userInput:", userInput);
   // \u0003 maps to ctrl+c input
   if (userInput === "\u0003") {
     process.exit();
+    return;
   }
-  if (userInput === "w") {
-    connection.write("Move: up");
-  }
-  if (userInput === "a") {
-    connection.write("Move: left");
-  }
-  if (userInput === "s") {
-    connection.write("Move: down");
-  }
-  if (userInput === "d") {
-    connection.write("Move: right");
-  }
-  if (userInput === " ") {
-    connection.write("Say: Move!");
-  }
+  connection.write(MOVES[userInput]);
+  // if (userInput === "w") {
+  //   connection.write("Move: up");
+  // }
+  // if (userInput === "a") {
+  //   connection.write("Move: left");
+  // }
+  // if (userInput === "s") {
+  //   connection.write("Move: down");
+  // }
+  // if (userInput === "d") {
+  //   connection.write("Move: right");
+  // }
+  // if (userInput === " ") {
+  //   connection.write("Say: Move!");
+  // }
 };
 
 // setup interface to handle user input from stdin
